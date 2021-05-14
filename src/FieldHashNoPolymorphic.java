8b5885c996465d446b0ed602030ea2bdeef78004
sunmengyuan <howdoyoudo0601@qq.com>
2021-05-14 14:43:03 +0800

    m

--- src/FieldHashNoPolymorphic.java
+++ src/FieldHashNoPolymorphic.java
@@ -6,7 +6,7 @@ public class FieldHashNoPolymorphic {
     static class Father {
         public int money = 1;
         public Father() {
-            money = 2;
+            money = 3;
             showMeTheMoney();
         }
         public void showMeTheMoney() {

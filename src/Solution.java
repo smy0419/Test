f27af6d96214724c3bde3214d9f29efde2c9c4fb
sunmengyuan <howdoyoudo0601@qq.com>
2021-07-05 09:39:49 +0800

    动态规划

---
+++ src/Solution.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+/**
+ * @author sunmengyuan
+ * @date 2021-07-03
+ * 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/coin-change
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+    public static int coinChange(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+
+    public static void main(String[] args) {
+        int[] x = {1, 3};
+        System.out.println(coinChange(x, 5));
+    }
+}

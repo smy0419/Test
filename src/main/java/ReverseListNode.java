a9d80353237dd0256e98372e02f1779e6e0dd47e
sunmengyuan <howdoyoudo0601@qq.com>
2021-07-07 14:59:31 +0800

    up

--- src/main/java/ReverseListNode.java
+++ src/main/java/ReverseListNode.java
@@ -17,12 +17,19 @@ public class ReverseListNode {
         node.next.next = new ListNode(3);
         node.next.next.next = new ListNode(4);
         node.next.next.next.next = new ListNode(5);
+        node.next.next.next.next.next = new ListNode(6);
 
-        ListNode x = reverse(node);
-        System.out.println(x);
+        ListNode x = reverseWithLoop(node);
+        System.out.println(x.val);
 
     }
 
+    /**
+     * 递归反转链表
+     *
+     * @param head
+     * @return
+     */
     static ListNode reverse(ListNode head) {
         if (head.next == null) {
             return head;
@@ -31,5 +38,23 @@ public class ReverseListNode {
         head.next.next = head;
         head.next = null;
         return last;
+    }
+
+    /**
+     * 循环反转链表
+     *
+     * @param head
+     * @return
+     */
+    static ListNode reverseWithLoop(ListNode head) {
+        ListNode pre = null;
+        ListNode next;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
     }
 }

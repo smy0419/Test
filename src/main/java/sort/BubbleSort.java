9095d8ebf508a1e7f3bae9f99a0977550bffdc89
sunmengyuan <howdoyoudo0601@qq.com>
2021-09-04 20:35:25 +0800

    112

--- src/main/java/sort/BubbleSort.java
+++ src/main/java/sort/BubbleSort.java
@@ -31,12 +31,12 @@ public class BubbleSort {
         if (nums == null || nums.length < 2) {
             return;
         }
-        for (int i = 1;i<nums.length;i++) {
-            for (int j=i;j>0;j--) {
-                if (nums[j] < nums[j-1]) {
-                    int temp = nums[j-1];
-                    nums[j-1] = nums[j];
-                    nums[j]=temp;
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (nums[j] < nums[j - 1]) {
+                    int temp = nums[j - 1];
+                    nums[j - 1] = nums[j];
+                    nums[j] = temp;
                 }
             }
         }

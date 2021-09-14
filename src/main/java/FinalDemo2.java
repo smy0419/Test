d03ac13dd4fd9c18e587a1bc642e1c3938de222f
sunmengyuan <howdoyoudo0601@qq.com>
2021-09-14 15:53:05 +0800

    add comment

--- src/main/java/FinalDemo2.java
+++ src/main/java/FinalDemo2.java
@@ -5,14 +5,27 @@
 public class FinalDemo2 {
     public static void main(String[] args) {
         MyClass myClass = new MyClass();
-        StringBuffer buffer = new StringBuffer("hello");
+        final StringBuffer buffer = new StringBuffer("hello");
         myClass.changeValue(buffer);
         System.out.println(buffer.toString());
     }
 }
 
+/**
+ * 修改buffer的引用，无效
+ */
+//class MyClass {
+//    void changeValue(StringBuffer buffer) {
+//        buffer = new StringBuffer("sd");
+//        buffer.append("world");
+//    }
+//}
+
+/**
+ * 没有修改buffer的引用，修改buffer的内容，有效
+ */
 class MyClass {
-    void changeValue(final StringBuffer buffer) {
+    void changeValue(StringBuffer buffer) {
         buffer.append("world");
     }
 }

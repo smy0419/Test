e662a8d629ba43cc87371623c2909077f9a1ac73
sunmengyuan <howdoyoudo0601@qq.com>
2021-10-24 19:09:56 +0800

    增加String常量池

--- src/main/java/FinalDemo.java
+++ src/main/java/FinalDemo.java
@@ -10,7 +10,9 @@ public class FinalDemo {
          * 当final变量是基本数据类型以及String类型时，如果在编译期间能知道它的确切值，则编译器会把它当做编译期常量使用。
          * 也就是说在用到该final变量的地方，相当于直接访问的这个常量，不需要在运行时确定。
          */
+        // 会放在常量池中，位于方法区/metaspace
         String a = "hello2";
+        // 因为在常量池中已经有hello2,所以a1与a指向同一个对象
         String a1 = "hello2";
         final String b = "hello";
         String d = "hello";
@@ -19,5 +21,28 @@ public class FinalDemo {
         System.out.println((a == c));
         System.out.println((a == e));
         System.out.println((a == a1));
+
+        /**
+         * 面试题：String str4 = new String(“abc”) 创建多少个对象？
+         * 在常量池中查找是否有“abc”对象
+         * 有则返回对应的引用实例
+         * 没有则创建对应的实例对象
+         * 在堆中 new 一个 String("abc") 对象
+         * 将对象地址赋值给str4,创建一个引用
+         * 所以，常量池中没有“abc”字面量则创建两个对象，否则创建一个对象，以及创建一个引用
+         * 根据字面量，往往会提出这样的变式题：
+         * String str1 = new String("A"+"B") ; 会创建多少个对象?
+         * String str2 = new String("ABC") + "ABC" ; 会创建多少个对象?
+         * str1：
+         * 字符串常量池："A","B","AB" : 3个
+         * 堆：new String("AB") ：1个
+         * 引用： str1 ：1个
+         * 总共 ： 5个
+         * str2 ：
+         * 字符串常量池："ABC" : 1个
+         * 堆：new String("ABC") ：1个
+         * 引用： str2 ：1个
+         * 总共 ： 3个
+         */
     }
 }
